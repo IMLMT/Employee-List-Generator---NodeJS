b60aa48caccfc103936f4e8dd0c802d2c42e3ad8
unknown <lovinmathew@hotmail.com>
2020-03-06 10:46:01 -0600

    Changed

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ async function generateHTML() {
     
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <title>Read-Me PDF</title>
+        <title>Employee List</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script defer src="index.js"></script>
         <script defer src="script.js"></script>
